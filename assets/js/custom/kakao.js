b0287a439c729a080258b4e39198b35d0ce5aa5a
kyh0703 <kyh0703@nate.com>
2021-09-28 16:03:56 +0900

    카카오 공유 추가

--- assets/js/custom/kakao.js
+++ assets/js/custom/kakao.js
@@ -1,12 +1,11 @@
-function sendLink() {
-  console.log("hihihihihi")
-  Kakao.init("85708b5cb536e693138f21a784bc7b25");
-  Kakao.Link.sendCustom({
+function sendLink(title, description, url) {
+    Kakao.init("85708b5cb536e693138f21a784bc7b25");
+    Kakao.Link.sendCustom({
     templateId: 62424, // 복사해둔 템플릿 ID
     templateArgs: {
-      title: "{{ page.title }}",
-      description: "{{ page.excerpt }}",
-      url: "{{ page.url }}",
+        title: "{{ page.title }}",
+        description: "{{ page.excerpt }}",
+        url: "{{ page.url }}",
     },
-  });
+    });
 }
