21bc39c84f82131d6094dc62cbca5abfa093c566
kyh0703 <kyh0703@nate.com>
2021-09-28 16:09:49 +0900

    카카오 공유 추가

--- assets/js/custom/kakao.js
+++ assets/js/custom/kakao.js
@@ -7,7 +7,7 @@ function sendLink(page_title, page_description, page_url) {
         Kakao.init("85708b5cb536e693138f21a784bc7b25");
 
     Kakao.Link.sendCustom({
-    templateId: 62424, // 복사해둔 템플릿 ID
+    templateId: 62424,
     templateArgs: {
         title: page_title,
         description: page_description,
