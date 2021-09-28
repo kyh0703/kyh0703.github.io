92cd01453095e34382c468e48f5c7a2216641c98
kyh0703 <kyh0703@nate.com>
2021-09-28 15:07:10 +0900

    카카오 공유 추가

---
+++ assets/js/custom/kakao.js
@@ -0,0 +1,12 @@
+function sendLink() {
+  console.log("hihihihihi")
+  Kakao.init("85708b5cb536e693138f21a784bc7b25");
+  Kakao.Link.sendCustom({
+    templateId: 62424, // 복사해둔 템플릿 ID
+    templateArgs: {
+      title: "{{ page.title }}",
+      description: "{{ page.excerpt }}",
+      url: "{{ page.url }}",
+    },
+  });
+}
