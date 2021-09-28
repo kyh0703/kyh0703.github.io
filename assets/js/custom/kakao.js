d691a76a5a8a0e06687320028c5397af693c35fa
kyh0703 <kyh0703@nate.com>
2021-09-28 16:09:36 +0900

    카카오 공유 추가

--- assets/js/custom/kakao.js
+++ assets/js/custom/kakao.js
@@ -1,5 +1,9 @@
 function sendLink(page_title, page_description, page_url) {
-    if (!Kakao.isinitialized())
+    console.log(page_title);
+    console.log(page_description);
+    console.log(page_url);
+
+    if (!Kakao.isInitialized())
         Kakao.init("85708b5cb536e693138f21a784bc7b25");
 
     Kakao.Link.sendCustom({
